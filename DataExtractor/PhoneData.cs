ba122c7b19db544bfb3c5245a24e9953d9f36e22
crispybee <crispybee9@gmail.com>
2016-10-19 13:56:01 +0200

    initial commit

---
+++ DataExtractor/PhoneData.cs
@@ -0,0 +1,53 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="PhoneData.cs" company="-">
+//   Tim Schlagenhaufer
+// </copyright>
+// <summary>
+//   The phone data.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace DataExtractor
+{
+    using System;
+
+    /// <summary>
+    /// The phone data.
+    /// </summary>
+    public class PhoneData
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PhoneData"/> class.
+        /// </summary>
+        /// <param name="timestamp">
+        /// The timestamp.
+        /// </param>
+        /// <param name="mac">
+        /// The mac.
+        /// </param>
+        /// <param name="distance">
+        /// The distance.
+        /// </param>
+        public PhoneData(string timestamp, string mac, string distance)
+        {
+            this.Timestamp = long.Parse(timestamp);
+            this.Mac = mac;
+            this.Distance = double.Parse(distance);
+        }
+
+        /// <summary>
+        /// Gets the timestamp.
+        /// </summary>
+        public long Timestamp { get; }
+
+        /// <summary>
+        /// Gets the mac.
+        /// </summary>
+        public string Mac { get; }
+
+        /// <summary>
+        /// Gets the distance.
+        /// </summary>
+        public double Distance { get; }
+    }
+}
---
+++ DataExtractor/Program.cs
@@ -0,0 +1,89 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="-">
+//   Tim Schlagenhaufer
+// </copyright>
+// <summary>
+//   Defines the Program type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace DataExtractor
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    /// <summary>
+    /// The program.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// The main.
+        /// </summary>
+        /// <param name="args">
+        /// The args.
+        /// </param>
+        private static void Main(string[] args)
+        {
+            var reader = new StreamReader(File.OpenRead(@"VS.csv"));
+            
+            var timestamp = new List<string>();
+            var id = new List<string>();
+            var mac = new List<string>();
+            var distance = new List<string>();
+
+            var andiPhoneData = new List<PhoneData>();
+            var timPhoneData = new List<PhoneData>();
+
+            // Get only relevant values
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                var values = line.Split(';');
+
+                timestamp.Add(values[1]);
+                id.Add(values[2]);
+                mac.Add(values[4]);
+                distance.Add(values[5]);
+            }
+
+            // Split phones to individual lists
+            for (int i = 0; i < timestamp.Count; i++)
+            {
+                if (id[i] == "Tim")
+                {
+                    timPhoneData.Add(new PhoneData(timestamp[i], mac[i], distance[i]));
+                }
+                else if (id[i] == "Andi")
+                {
+                    andiPhoneData.Add(new PhoneData(timestamp[i], mac[i], distance[i]));
+                }
+            }
+
+            var macListArray = new List<List<PhoneData>>();
+
+            // group list by mac adress
+            foreach (var phoneDataMacList in timPhoneData.GroupBy(x => x.Mac))
+            {
+                var groupedMacList = phoneDataMacList.ToList();
+                macListArray.Add(groupedMacList);
+            }
+
+            for (int i = 0; i < macListArray.Count; i++)
+            {
+                var writer = new StreamWriter(File.OpenWrite(i + @".csv"));
+
+                foreach (var phoneData in macListArray[i])
+                {
+                    writer.WriteLine(phoneData.Timestamp + ";" + phoneData.Mac + ";" + phoneData.Distance);
+                }
+
+                writer.Close();
+            }
+            
+            Console.WriteLine("END");
+        }
+    }
+}
